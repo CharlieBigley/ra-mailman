4c16998fa7c8d34332c07af1fba690b9a2642c3d
Charlie Bigley <charlie@bigley.me.uk>
2025-11-15 10:22:09 +0000

    Version 4.5.8

--- com_ra_mailman/administrator/src/View/Mail_lsts/HtmlView.php
+++ com_ra_mailman/administrator/src/View/Mail_lsts/HtmlView.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @version    4.2.0
+ * @version    4.5.3
  * @package    com_ra_mailman
  * @author     Charlie Bigley <webmaster@bigley.me.uk>
  * @copyright  2023 Charlie Bigley
@@ -13,6 +13,9 @@
  * 13/02/25 CB replace getIdentity with $this->getCurrentUser
  * 18/02/25 CB always show Cancel button
  * 20/03/25 CB Return to Dashboard
+ * 06/-8/25 CB warning if email logging is active
+ * 11/08/25 CB new mechanism for send
+ * 25/08/25 CB Help
  */
 
 namespace Ramblers\Component\Ra_mailman\Administrator\View\Mail_lsts;
@@ -31,7 +34,8 @@ use \Joomla\CMS\Form\Form;
 use \Joomla\CMS\HTML\Helpers\Sidebar;
 use \Joomla\CMS\User\CurrentUserInterface;
 use \Joomla\Component\Content\Administrator\Extension\ContentComponent;
-use \Ramblers\Component\Ra_tools\Site\Helpers\ToolsHelper;
+use Ramblers\Component\Ra_mailman\Site\Helpers\Mailhelper;
+use Ramblers\Component\Ra_tools\Site\Helpers\ToolsHelper;
 
 /**
  * View class for a list of Mail_lists.
@@ -42,79 +46,14 @@ class HtmlView extends BaseHtmlView implements CurrentUserInterface {
 
     protected $canDo;
     protected $items;
+    protected $mailshot_send_message;
+    protected $max_online_send;
+    public $mailHelper;
+    public $toosHelper;
     protected $pagination;
     protected $state;
     protected $user;
     protected $user_id;
-
-    function defineActions($list_id, $list_type, $unsent_mailshot, $mailshot_id) {
-        /*
-         * invoked from the template to set up the required action buttons for the last column of the report
-         * 01/01/24 CB this seems to be unused - code copied from site view?
-         */
-
-        if ($this->user_id == 0) {
-            return '';
-        }
-        $objMailHelper = new Ra_mailmanMailhelper;
-        $objHelper = new ToolsHelper;
-
-        if ($objMailHelper->isAuthor($list_id)) {
-            $target = 'index.php?option=com_ra_mailman&task=mailshot.edit';
-            if ($unsent_mailshot == true) {
-                $caption = 'Edit';
-                $target .= '&id=' . $mailshot_id;
-            } else {
-                $caption = 'Create';
-            }
-// this will over-ride any options already set
-            return $objHelper->buildLink($target . '&list_id=' . $list_id, $caption, False, "link-button button-p0159");
-        }
-
-// See if User is already subscribed
-        $sql = 'SELECT id, state FROM #__ra_mail_subscriptions WHERE user_id=' . $this->user_id;
-        $sql .= ' AND list_id=' . $list_id;
-        $subscription = $objHelper->getItem($sql);
-        if ($list_type == 'Open') {
-            if (is_null($subscription)) {
-                $target = 'index.php?option=com_ra_mailman&task=mail_lst.subscribe';
-                $caption = 'Subscribe';
-                $colour = '0583';  // light green
-            } else {
-//                echo "View: state $subscription->state<br>";
-                if ($subscription->state == 0) {
-                    $target = 'index.php?option=com_ra_mailman&task=mail_lst.subscribe';
-                    $caption = 'Re-subscribe';
-                    $colour = '0555';  // dark green
-                }
-            }
-        }
-// For unsubscribing, we don't care if the list is open or closed
-        if (!is_null($subscription)) {
-            if ($subscription->state > 0) {
-                $target = 'index.php?option=com_ra_mailman&task=mail_lst.unsubscribe&list_id=';
-                $caption = 'Un-subscribe';
-                $colour = '0186'; // red
-                return $objHelper->buildLink($target . $list_id . '&user_id=' . $this->user_id, $caption, False, "link-button button-p" . $colour);
-            }
-        }
-        if ($list_type == 'Open') {
-            if (is_null($subscription)) {
-                $target = 'index.php?option=com_ra_mailman&task=mail_lst.subscribe';
-                $caption = 'Subscribe';
-                $colour = '0583';  // light green
-            } else {
-                if ($subscription->state == 0) {
-                    $target = 'index.php?option=com_ra_mailman&task=mail_lst.subscribe';
-                    $caption = 'Re-subscribe';
-                    $colour = '0555';  // dark green
-                }
-            }
-            return $objHelper->buildLink($target . '&list_id=' . $list_id . '&user_id=' . $this->user_id, $caption, False, "link-button button-p" . $colour);
-        } else {
-            return '';
-        }
-    }
 
     /**
      * Display the view
@@ -141,6 +80,17 @@ class HtmlView extends BaseHtmlView implements CurrentUserInterface {
 
         $this->addToolbar();
         $this->sidebar = Sidebar::render();
+        $params = ComponentHelper::getParams('com_ra_tools');
+        $email_log_level = $params->get('email_log_level', '0');
+        // Log level -2 is solely to benchmark the overhead of sending via SMTP
+        if ($email_log_level < 0) {
+            Factory::getApplication()->enqueueMessage('Email logging is in Benchmark mode: ' . $email_log_level, 'warning');
+        }
+        $params = ComponentHelper::getParams('com_ra_mailman');
+        $this->max_online_send = $params->get('max_online_send', 100);
+        $this->mailshot_send_message = $params->get('mailshot_send_message', 'Waiting for batch job');
+        $this->mailHelper = new MailHelper;
+        $this->toolsHelper = new ToolsHelper;
         parent::display($tpl);
     }
 
@@ -152,10 +102,16 @@ class HtmlView extends BaseHtmlView implements CurrentUserInterface {
      * @since   1.0.6
      */
     protected function addToolbar() {
+        $app = Factory::getApplication();
 // Suppress menu side panel
-        Factory::getApplication()->input->set('hidemainmenu', true);
+        $app->input->set('hidemainmenu', true);
+// Set sidebar action
+        Sidebar::setAction('index.php?option=com_ra_mailman&view=mail_lsts');
         $state = $this->get('State');
         $this->canDo = ContentHelper::getActions('com_ra_mailman');
+        // find which layout is in use
+
+        $layout = $app->input->getCmd('layout', 'new');
         if ($layout == 'statistics') {
             if (!$this->canDo->get('core.delete')) {
                 throw new \Exception('Access denied', 404);
@@ -209,8 +165,8 @@ class HtmlView extends BaseHtmlView implements CurrentUserInterface {
                     ->listCheck(false);
         }
         ToolbarHelper::cancel('mail_lsts.cancel', 'Return to Dashboard');
-// Set sidebar action
-        Sidebar::setAction('index.php?option=com_ra_mailman&view=mail_lsts');
+        $help_url = 'https://docs.stokeandnewcastleramblers.org.uk/mail-manager.html?view=article&id=420:mm-02-2-mailing-lists&catid=34';
+        ToolbarHelper::help('', false, $help_url);
     }
 
     /**
@@ -237,6 +193,97 @@ class HtmlView extends BaseHtmlView implements CurrentUserInterface {
      */
     public function getState($state) {
         return isset($this->state->{$state}) ? $this->state->{$state} : false;
+    }
+
+    function defineActions($list_id, $list_type, $unsent_mailshot, $mailshot_id) {
+        /*
+         * invoked from the template to set up the required action buttons for the last column of the report
+         * 01/01/24 CB this seems to be unused - code copied from site view?
+         */
+
+        if ($this->user_id == 0) {
+            return '';
+        }
+
+        if ($this->mailHelper->isAuthor($list_id)) {
+            $target = 'index.php?option=com_ra_mailman&task=mailshot.edit';
+            if ($unsent_mailshot == true) {
+                $caption = 'Edit';
+                $target .= '&id=' . $mailshot_id;
+            } else {
+                $caption = 'Create';
+            }
+// this will over-ride any options already set
+            return $this->toolsHelper->buildLink($target . '&list_id=' . $list_id, $caption, False, "link-button button-p0159");
+        }
+
+// See if User is already subscribed
+        $sql = 'SELECT id, state FROM #__ra_mail_subscriptions WHERE user_id=' . $this->user_id;
+        $sql .= ' AND list_id=' . $list_id;
+        $subscription = $this->toolsHelper->getItem($sql);
+        if ($list_type == 'Open') {
+            if (is_null($subscription)) {
+                $target = 'index.php?option=com_ra_mailman&task=mail_lst.subscribe';
+                $caption = 'Subscribe';
+                $colour = '0583';  // light green
+            } else {
+//                echo "View: state $subscription->state<br>";
+                if ($subscription->state == 0) {
+                    $target = 'index.php?option=com_ra_mailman&task=mail_lst.subscribe';
+                    $caption = 'Re-subscribe';
+                    $colour = '0555';  // dark green
+                }
+            }
+        }
+// For unsubscribing, we don't care if the list is open or closed
+        if (!is_null($subscription)) {
+            if ($subscription->state > 0) {
+                $target = 'index.php?option=com_ra_mailman&task=mail_lst.unsubscribe&list_id=';
+                $caption = 'Un-subscribe';
+                $colour = '0186'; // red
+                return $this->toolsHelper->buildLink($target . $list_id . '&user_id=' . $this->user_id, $caption, False, "link-button button-p" . $colour);
+            }
+        }
+        if ($list_type == 'Open') {
+            if (is_null($subscription)) {
+                $target = 'index.php?option=com_ra_mailman&task=mail_lst.subscribe';
+                $caption = 'Subscribe';
+                $colour = '0583';  // light green
+            } else {
+                if ($subscription->state == 0) {
+                    $target = 'index.php?option=com_ra_mailman&task=mail_lst.subscribe';
+                    $caption = 'Re-subscribe';
+                    $colour = '0555';  // dark green
+                }
+            }
+            return $this->toolsHelper->buildLink($target . '&list_id=' . $list_id . '&user_id=' . $this->user_id, $caption, False, "link-button button-p" . $colour);
+        } else {
+            return '';
+        }
+    }
+
+    protected function sendButton($last_mailshot, $count_subscribers, $canEdit, $isAuthor) {
+        //      print_r($last_mailshot);
+        //      echo '<br>';
+        $target_send = 'administrator/index.php?option=com_ra_mailman&task=mailshot.send&menu_id=' . $this->menu_id . '&mailshot_id=';
+        if (($last_mailshot->id > 0) AND is_null($last_mailshot->date_sent)) {
+            if (($canEdit) OR ($isAuthor)) {
+                if ($last_mailshot->attachment != '') {
+                    $target = 'images/com_ra_mailman/' . $last_mailshot->attachment;
+                    $label = '<span class="icon-paperclip"></span>';
+                    echo $this->toolsHelper->buildLink($target, $label, True);
+                }
+                $target = $target_send . $last_mailshot->id . '&total=' . $count_subscribers;
+//                                           $target = 'administrator/index.php?option=com_ra_mailman&task=mailshot.send&mailshot_id='  . '&menu_id=' . $this->menu_id;
+                if (is_null($last_mailshot->processing_started)) {
+                    $label = 'Send';
+                } else {
+                    $count = $this->mailHelper->countSubscribersOutstanding($last_mailshot->id);
+                    $label = 'Resend to ' . $count;
+                }
+                return $this->toolsHelper->buildButton($target, $label, False);
+            }
+        }
     }
 
 }
