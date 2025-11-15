d48bc5cfb3f32274205bf48d85342814fa80f117
Charlie Bigley <charlie@bigley.me.uk>
2025-11-15 10:24:43 +0000

    Version 4.5.8

--- com_ra_mailman/administrator/src/View/Subscriptions/HtmlView.php
+++ com_ra_mailman/administrator/src/View/Subscriptions/HtmlView.php
@@ -1,15 +1,13 @@
 <?php
 
 /**
- * @version    4.0.10
+ * @version    4.5.3
  * @package    com_ra_mailman
- * @author     Charlie Bigley <webmaster@bigley.me.uk>
- * @copyright  2023 Charlie Bigley
+ * @author     Charlie Bigley <charlie@bigley.me.uk>
+ * @copyright  2025 Charlie Bigley
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
- * 19/09/23 CB use MailHelper from Site, not Administrator
- * 01/01/24 CB use ContentHelper->getActions
- * 19/02/25 CB implement CurrentUserInterface
- * 20/03/25 CB Return to Dashboard
+ * 17/07/25 CB regenerated, adjust buttons, use ContentHelp and CurrentUserInterface
+ * 25/08/25 CB Help
  */
 
 namespace Ramblers\Component\Ra_mailman\Administrator\View\Subscriptions;
@@ -17,22 +15,20 @@ namespace Ramblers\Component\Ra_mailman\Administrator\View\Subscriptions;
 // No direct access
 defined('_JEXEC') or die;
 
-use \Joomla\CMS\Factory;
+use \Joomla\Component\Content\Administrator\Extension\ContentComponent;
 use Joomla\CMS\Helper\ContentHelper;
 use \Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
 use \Joomla\CMS\Toolbar\Toolbar;
 use \Joomla\CMS\Toolbar\ToolbarHelper;
 use \Joomla\CMS\Language\Text;
+use \Joomla\CMS\Form\Form;
 use \Joomla\CMS\HTML\Helpers\Sidebar;
 use \Joomla\CMS\User\CurrentUserInterface;
-use \Joomla\Component\Content\Administrator\Extension\ContentComponent;
-use \Ramblers\Component\Ra_tools\Site\Helpers\ToolsHelper;
-use \Ramblers\Component\Ra_mailman\Site\Helpers\MailHelper;
 
 /**
  * View class for a list of Subscriptions.
  *
- * @since  1.0.4
+ * @since  4.4.11
  */
 class HtmlView extends BaseHtmlView implements CurrentUserInterface {
 
@@ -56,12 +52,12 @@ class HtmlView extends BaseHtmlView implements CurrentUserInterface {
         $this->pagination = $this->get('Pagination');
         $this->filterForm = $this->get('FilterForm');
         $this->activeFilters = $this->get('ActiveFilters');
-
+        $this->user = $this->getCurrentUser();
         // Check for errors.
         if (count($errors = $this->get('Errors'))) {
             throw new \Exception(implode("\n", $errors));
         }
-        $this->user = $this->getCurrentUser();
+
         $this->addToolbar();
 
         $this->sidebar = Sidebar::render();
@@ -73,35 +69,61 @@ class HtmlView extends BaseHtmlView implements CurrentUserInterface {
      *
      * @return  void
      *
-     * @since   1.0.4
+     * @since   4.4.11
      */
     protected function addToolbar() {
-        // Suppress menu side panel
-        Factory::getApplication()->input->set('hidemainmenu', true);
+        // Set sidebar action
+        Sidebar::setAction('index.php?option=com_ra_mailman&view=subscriptions');
         $state = $this->get('State');
         $canDo = ContentHelper::getActions('com_ra_mailman');
 
-        ToolbarHelper::title(Text::_('Subscriptions'), "generic");
+        ToolbarHelper::title(Text::_('Mail Subscriptions'), "generic");
 
         $toolbar = Toolbar::getInstance('toolbar');
-        /*
-          if ($canDo->get('core.edit.state')) {
-          $dropdown = $toolbar->dropdownButton('status-group')
-          ->text('JTOOLBAR_CHANGE_STATUS')
-          ->toggleSplit(false)
-          ->icon('fas fa-ellipsis-h')
-          ->buttonClass('btn btn-action')
-          ->listCheck(true);
 
-          $childBar = $dropdown->getChildToolbar();
+        if ($canDo->get('core.edit.state')) {
+            $dropdown = $toolbar->dropdownButton('status-group')
+                    ->text('JTOOLBAR_CHANGE_STATUS')
+                    ->toggleSplit(false)
+                    ->icon('fas fa-ellipsis-h')
+                    ->buttonClass('btn btn-action')
+                    ->listCheck(true);
 
-          if (isset($this->items[0]->state)) {
-          $childBar->publish('subscriptions.publish')->listCheck(true);
-          $childBar->unpublish('subscriptions.unpublish')->listCheck(true);
-          }
-          }
-         *
-         */
+            $childBar = $dropdown->getChildToolbar();
+
+            if (isset($this->items[0]->state)) {
+                $childBar->publish('subscriptions.publish')->listCheck(true);
+                $childBar->unpublish('subscriptions.unpublish')->listCheck(true);
+//                $childBar->archive('subscriptions.archive')->listCheck(true);
+//            } elseif (isset($this->items[0])) {
+//                // If this component does not use state then show a direct delete button as we can not trash
+//                $toolbar->delete('subscriptions.delete')
+//                        ->text('JTOOLBAR_EMPTY_TRASH')
+//                        ->message('JGLOBAL_CONFIRM_DELETE')
+//                        ->listCheck(true);
+//            }
+
+
+                if (isset($this->items[0]->checked_out)) {
+                    $childBar->checkin('subscriptions.checkin')->listCheck(true);
+                }
+
+//            if (isset($this->items[0]->state)) {
+//                $childBar->trash('subscriptions.trash')->listCheck(true);
+//            }
+            }
+
+
+
+            // Show trash and delete for components that uses the state field
+//        if (isset($this->items[0]->state)) {
+//            if ($this->state->get('filter.state') == ContentComponent::CONDITION_TRASHED && $canDo->get('core.delete')) {
+//                $toolbar->delete('subscriptions.delete')
+//                        ->text('JTOOLBAR_EMPTY_TRASH')
+//                        ->message('JGLOBAL_CONFIRM_DELETE')
+//                        ->listCheck(true);
+//            }
+        }
         $toolbar->standardButton('nrecords')
                 ->icon('fa fa-info-circle')
                 ->text(number_format($this->pagination->total) . ' Records')
@@ -109,9 +131,22 @@ class HtmlView extends BaseHtmlView implements CurrentUserInterface {
                 ->onclick('return false')
                 ->listCheck(false);
         ToolbarHelper::cancel('subscriptions.cancel', 'Return to Dashboard');
+        $help_url = 'https://docs.stokeandnewcastleramblers.org.uk/mail-manager.html?view=article&id=432:mm-02-4-subscriptions&catid=34';
+        ToolbarHelper::help('', false, $help_url);
+    }
 
-        // Set sidebar action
-        Sidebar::setAction('index.php?option=com_ra_mailman&view=subscriptions');
+    /**
+     * Method to order fields
+     *
+     * @return void
+     */
+    protected function getSortFields() {
+        return array(
+            'a.`id`' => Text::_('JGRID_HEADING_ID'),
+            'a.`state`' => Text::_('JSTATUS'),
+            'a.`user_id`' => Text::_('COM_RA_MAILMAN_SUBSCRIPTIONS_USER_ID'),
+            'a.`list_id`' => Text::_('COM_RA_MAILMAN_SUBSCRIPTIONS_LIST_ID'),
+        );
     }
 
     /**
